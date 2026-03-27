1f569a12ca484f1faa2c441343dd5dc1b1bbf998
lgills <pearlie30@yahoo.com>
2026-03-27 18:56:01 +0000

    Deploying to gh-pages from @ lgills/lgills.github.io@1cd30cdd833f8ee7e1916f5ec25b3c18e61abff9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,7 +451,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-unit-1-assignment",
+          title: 'Unit 1 Assignment',
+          description: "Demonstration of CLT",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Unit1Assignment_files/Unit1Assignment.md/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
